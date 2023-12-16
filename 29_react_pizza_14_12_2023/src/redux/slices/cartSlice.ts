27a1a043b68bef6f78847fc267b6cc790195f87a
Tetiana Ilienko <tatyanailyenko@gmail.com>
2023-12-16 19:10:33 +0100

    added Lesson 29

---
+++ 29_react_pizza_14_12_2023/src/redux/slices/cartSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface IState {
+    totalPrice: number,
+    items: { id: string, count: number, price:number }[]
+}
+const initialState: IState = {
+    totalPrice: 0,
+    items: []
+}
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+
+        addToCart: (state, action) => {
+            const findeItem = state.items.find(
+                (item) => item.id === action.payload.id
+            );
+
+            if (findeItem) {
+                findeItem.count++;
+            } else {
+                state.items.push({
+                    ...action.payload,
+                    count: 1
+            })
+
+            state.totalPrice = state.items.reduce(
+                (acc,item) => acc + item.price * item.count, 0
+            );
+            }
+
+
+
+
+        }
+
+    }
+})
